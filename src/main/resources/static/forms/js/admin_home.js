c4e13ed3150c29c22087f7adcc982d0b2cd28b95
erroshankr <roshankumarjha24@gmail.com>
2024-02-08 18:25:31 +0530

    image related changes

--- src/main/resources/static/forms/js/admin_home.js
+++ src/main/resources/static/forms/js/admin_home.js
@@ -1,41 +1,39 @@
+   $(document).ready(function() {
     $('#addDirectionBtn').click(function() {
-        var index = 1;
+        var index = $('.direction-row').length + 1;
         $('#directionFields').append(
             '<div class="direction-row">' +
             '<input type="text" name="directions[' + index + ']" placeholder="Direction ' + index + '" />' +
             '</div>'
         );
-        index++;
     });
 
      $('#addCategoryBtn').click(function() {
-            var index = 1;
-            $('#categoryFields').append(
+         var index = $('.category-row').length + 1;
+         $('#categoryFields').append(
                 '<div class="category-row">' +
                 '<input type="text" name="categories[' + index + ']" placeholder="Category ' + index + '" />' +
                 '</div>'
             );
-            index++;
         });
 
     $('#addIngredientBtn').click(function() {
-        var index = 1;
+        var index = $('.ingredient-row').length + 1;
         $('#ingredientDiv').append(
             '<div class="ingredient-row">' +
             '<input type="text" name="ingredients[' + index + '].key" placeholder="name ' + index + '"/>' +
             '<input type="text" name="ingredients[' + index + '].value" placeholder="qty ' + index + '"/>' +
             '</div>'
         );
-        index++;
     });
 
     $('#addNutritionalContentBtn').click(function() {
-        var index = 1;
+        var index = $('.nutritional-content-row').length + 1;
         $('#nutritionalContentFields').append(
             '<div class="nutritional-content-row">' +
             '<input type="text" name="nutritionalContents[' + index + '].key" placeholder="Content ' + index + '"/>' +
             '<input type="text" name="nutritionalContents[' + index + '].value" placeholder="Value ' + index + '"/>' +
             '</div>'
         );
-        index++;
     });
+});
