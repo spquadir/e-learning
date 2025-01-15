e57f717d2e30d47f6473d6d848cbcf26cc1f0811
Tushar166 <tusharbhaskar.1116@gmail.com>
2025-01-15 19:40:03 +0530

    login and user creation page

--- src/main/java/com/example/onlinelearning/models/UserModel.java
+++ src/main/java/com/example/onlinelearning/models/UserModel.java
@@ -22,23 +22,83 @@ public class UserModel {
     @Column(name = "surname")
     private String surname;
     @Column(name = "email")
-    private String email;
+  private String email;
     private String detail;
     private String imgUrl;
     @Column(name = "registration_date")
     private LocalDate date;
 
-    public UserModel(String username, String password, String name, String surname, String email, String imgUrl, LocalDate date) {
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+   public String getEmail() {
+        return email;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
-    public UserModel(String detail) {
-        this.detail = detail;
+    public LocalDate getDate() {
+        return date;
     }
+
+
 }
